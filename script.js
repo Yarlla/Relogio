08f0b1d7dd6a1a763408a86cd8f20ba711d61b8f
Yarlla <165423141+Yarlla@users.noreply.github.com>
2025-02-02 20:10:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const cidade = 'Cedro'; 
 const unidadeMedida = 'metric'; // unidade da temperatura
-const api_key = '9dac1a61ad91c38c986f1fc63aa0c1a1'; //chave da api 
-
+const apiKey = '9dac1a61ad91c38c986f1fc63aa0c1a1'; // chave da API 
 
 var h = document.getElementById('horas'); 
 var min = document.getElementById('minutos');
 var seg = document.getElementById('segundos');
+const dataHoje = document.getElementById('data');
+const diaSemana = document.getElementById('dia');
+const temperaturaHoje = document.getElementById('temperatura');
+const iconeClima = document.getElementById('icone-clima');
 
-// setInterval - executa uma função repetidamente em intervalos regulares
-const relogio = setInterval(function time() {
+const relogio = setInterval(function time() { // setInterval - executa uma função repetidamente em intervalos regulares
     let horarioAtual = new Date();
     let horas = horarioAtual.getHours();
     let minutos = horarioAtual.getMinutes();
@@ -25,41 +27,54 @@ const relogio = setInterval(function time() {
         segundos = '0' + segundos;
     }
 
-    // textContent - define o conteúdo de texto do elemento HTML
-    h.textContent = horas;
+    h.textContent = horas; // textContent - define o conteúdo de texto do elemento HTML
     min.textContent = minutos;
-    seg.textContent = segundos;        
-},1000)
+    seg.textContent = segundos;
+}, 1000);
 
-const dataHoje = document.getElementById('data');
 const data = setInterval(function time() {
     let dH = new Date();
-    // toLocaleDateString() - Para formatar a data de acordo com um idioma em especifico
-    let dataFormatada = dH.toLocaleDateString(); 
+    let dataFormatada = dH.toLocaleDateString();  // toLocaleDateString() - Para formatar a data de acordo com um idioma em especifico
     dataHoje.textContent = dataFormatada; 
-})
+});
 
-const diaSemana = document.getElementById('dia');
-const dia = setInterval(function time(){
+const dia = setInterval(function time() { 
     let d = new Date();
-    let diaDaSemana = d.toLocaleDateString('pt-BR', { weekday: 'long' }); // long é para colocar o nome completo da semana, por exemplo: terça-feira
-    // diaDaSemana[0].toUpperCase() - pega o primeiro caractere do dia da semana e converte para maiscula
-    // diaDaSemana.slice(1); - cria uma nova string a partir da segunda letra: erça-feira e junta com a letra maiscula
-    diaSemana.textContent = diaDaSemana[0].toUpperCase() + diaDaSemana.slice(1);
-})
+    let diaDaSemana = d.toLocaleDateString('pt-BR', { weekday: 'long' });
+    diaSemana.textContent = diaDaSemana[0].toUpperCase() + diaDaSemana.slice(1); // Primeira letra em maiúsculo e o resto em minúsculo para formatar o dia da semana
+});
 
-// Função para consultar a temperatura do dia atual
+// Função para consultar a temperatura do dia atual e mudar o ícone de acordo com o horário e o clima
 setInterval(async function () {
-    const temperaturaHoje = document.getElementById('clima');
-    const response = await fetch( 
-        `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=${unidadeMedida}&appid=${api_key}&lang=pt_br` 
-    )
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=${unidadeMedida}&appid=${apiKey}&lang=pt_br`
+        );
+        
+        if (response.ok) {
+            const data = await response.json();
+            temperaturaHoje.textContent = `${Math.round(data.main.temp)}°C`; // aqui exibi a temperatura, usando placeholder, e arredonda o valor
+
+            // Atualiza o ícone diretamente
+            const icone = data.weather[0].icon; // icone do clima
+            iconeClima.src = `https://openweathermap.org/img/wn/${icone}@2x.png`; //adiciona o icone do clima ao elemento img
+           
+        } else {
+            console.error(`Falha na requisição: ${response.status}`);
+        }
+    } catch (error) {
+        console.error('Erro na requisição ou processamento dos dados:', error);
+    }
+});
+
+// Função para ativar o alarme
+function ativarAlarme() {
+    const audio = document.getElementById('audio');
+    audio.play(); // ativa o alarme  a partir do horario atual
     
-    if (response.ok){
-        const data = await response.json(); // Converte a temperatura da API em json
-        temperaturaHoje.textContent = `${Math.round(data.main.temp)}°C`; // aqui exibir a temperatura, usando placeholder, e arredondar o valor
-    }
-    else{
-        console.error(`Falha na requisição: ${response.status}`);
-    }
-},100) 
+    // toca durante 5 minutos e depois para
+    setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0;
+    }, 300000);
+}
